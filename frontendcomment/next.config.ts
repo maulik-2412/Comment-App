6709999309366b1f1e47b3f769fceb257395cb33
Maulik Tyagi <mauliktyagi08@gmail.com>
2025-07-10 03:03:56 +0530

    changed proxy

--- frontendcomment/next.config.ts
+++ frontendcomment/next.config.ts
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://backend:5000/:path*', // docker backend service
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
